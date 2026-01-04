9ba213a84f4ead1d858028aa8670da42f02ce5ff
Gabriele Marraccini <gabriele.marraccini95@gmail.com>
2026-01-04 23:24:09 +0100

    Allineamento icone a db

--- src/app/components/enums/exercise-icons.ts
+++ src/app/components/enums/exercise-icons.ts
@@ -4,14 +4,17 @@ export const DEFAULT_ICON_COLOR = '#fff'; // Grigio neutro
 // exercise-icons.enum.ts
 export const ExerciseIconPaths: Record<number, string> = {
   0: 'assets/recollect/images/default-icon.png', // Default/Fallback
-  1: 'assets/recollect/images/bicipite-icon.png',
-  2: 'assets/recollect/images/dorsali-icon.png',
-  3: 'assets/recollect/images/petto-icon.png',
+  1: 'assets/recollect/images/petto-icon.png',
+  2: 'assets/recollect/images/bicipite-icon.png',
+  3: 'assets/recollect/images/dorsali-icon.png',
   4: 'assets/recollect/images/spalle-icon.png',
-  5: 'assets/recollect/images/quadricipiti-icon.png',
-  6: 'assets/recollect/images/femorali-icon.png',
-  8: 'assets/recollect/images/core-icon.png',
-  9: 'assets/recollect/images/cardio-icon.png'
+  5: 'assets/recollect/images/tricipiti-icon.png',
+  6: 'assets/recollect/images/quadricipiti-icon.png',
+  7: 'assets/recollect/images/femorali-icon.png',
+  8: 'assets/recollect/images/glutei-icon.png',
+  9: 'assets/recollect/images/core-icon.png',
+  10: 'assets/recollect/images/cardio-icon.png',
+  11: 'assets/recollect/images/default-icon.png', 
   // Aggiungi altri mapping se aggiungi icone nel DB e carichi i file in assets
 }; 
 
