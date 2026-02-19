b02f0f581ff1b5ee6a88411cbc0e385652864024
Gabriele Marraccini <gabriele.marraccini95@gmail.com>
2026-02-19 23:00:24 +0100

    Release 1.1.48

--- src/app/core/config/app-info.config.ts
+++ src/app/core/config/app-info.config.ts
@@ -2,7 +2,7 @@
 // Le informazioni provengono dal package.json
 
 export const APP_INFO = {
-  version: '1.1.48',
+  version: '1.1.49',
   name: 'gymshark-frontend',
-  buildDate: '2026-02-16T21:47:08.656Z'
+  buildDate: '2026-02-19T21:56:54.480Z'
 };
